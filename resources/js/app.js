4d63dd66dce3c712a80e53af6b699ddd1e5f60fe
manojchrsya <manojchrsya@gmail.com>
2021-06-30 10:24:24 +0530

    added login and signup form

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '../css/app.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../css/app.css';
 
-import 'jquery/dist/jquery.min.js'
-import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
